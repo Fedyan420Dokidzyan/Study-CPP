e04d793f8faec813ebb11896e7d5ebc1e2ae3ebd
Fedyan420Dokidzyan <arenda4005@gmail.com>
2023-10-14 21:30:28 +0300

    second homework

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+#include "move_me_out.hpp"
+
+#include <iostream>
+
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file" << std::endl;
+}
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+
+	todo_move_me_out();
+	
+	return 0;
+}
---
+++ move_me_out.cpp
@@ -0,0 +1,7 @@
+#include "move_me_out.hpp"
+
+#include <iostream>
+
+void example_function() {
+	std::cout << "Hello from example function!" << std::endl;
+}
